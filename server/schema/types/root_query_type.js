b091c1a10911ed1c286a1df4593602aac6dd93bf
Omar <omar@kamelizer.com>
2021-12-28 18:23:41 +0200

    user root query

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,11 +1,17 @@
 const { GraphQLID } = require('graphql');
 const graphql = require('graphql');
+const UserType = require('./user_type');
 const { GraphQLObjectType } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields:{
-    dummyField:{type:GraphQLID}
+    user:{
+      type:UserType,
+      resolve(parentValue, args, req){
+        return req.user;
+      }
+    }
   }
 });
 
