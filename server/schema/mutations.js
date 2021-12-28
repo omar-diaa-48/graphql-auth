fbf02a0cfd97789b28896f6201a084993ca2f15d
Omar <omar@kamelizer.com>
2021-12-28 17:56:34 +0200

    add logout and login mutations

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -20,6 +20,25 @@ const mutation = new GraphQLObjectType({
                 const { email, password } = args;
                 return AuthService.signup({ email, password, req })
             }
+        },
+        logout:{
+            type:UserType,
+            resolve(parentValue, args, req){
+                const { user } = req;
+                req.logout();
+                return user;
+            }
+        },
+        login:{
+            type:UserType,
+            args:{
+                email:{type:GraphQLString},
+                password:{type:GraphQLString}
+            },
+            resolve(parentValue, args, req){
+                const {email, password} = args;
+                return AuthService.login({email, password, req})
+            }
         }
     }
 })
