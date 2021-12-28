dfd4f738fb8d7ef2b6fbf130629ebdfd33495e0d
Omar <omar@kamelizer.com>
2021-12-28 19:45:12 +0200

    add header buttons

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,21 +1,60 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
-import { currentUserQuery } from "../queries/CurrentUser";
+import { Link } from "react-router"; 
+import { currentUserQuery } from "../queries";
+import { logoutMutation } from "../mutations"
 
 class Header extends Component {
-    render(){
-        console.log(this.props.data);
+    handleLogout(){
+        this.props.mutate({
+            refetchQueries:[{query:currentUserQuery}]
+        });
+    }
 
-        if(this.props.loading){
-            return <div>Loading....</div>
+    renderButtons(){
+        const {user, loading} = this.props.data;
+
+        if(loading){
+            return <div />
         }
 
-        return(
+        if(user){
+            return (
+                <li>
+                    <a onClick={this.handleLogout.bind(this)}>
+                        Logout
+                    </a>
+                </li>
+            )
+        }
+
+        return (
             <div>
-                Header
+                <li>
+                    <Link to="/signup">Signup</Link>
+                </li>
+                <li>
+                    <Link to="/signin">Signin</Link>
+                </li>
             </div>
+        )
+    }
+
+    render(){
+
+        return(
+            <nav>
+                <div className="nav-wrapper" >
+                    <Link to="/" >Home</Link>
+                    <ul className="right" >
+                        {this.renderButtons()}
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
 
-export default graphql(currentUserQuery)(Header);
+export default graphql(logoutMutation)(
+    graphql(currentUserQuery)(Header)
+)
